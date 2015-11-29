9df0da19e4b22322c4a6365e679986454e20de1b
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2015-11-29 10:26:36 +0100

    add tests

--- howl/tests/test_admin.py
+++ howl/tests/test_admin.py
@@ -1,13 +1,29 @@
 import pytest
 from django.contrib import admin
 
-from howl.admin import ObserverAdmin
-from howl.models import Observer
+from howl.admin import AlertAdmin, ObserverAdmin
+from howl.models import Alert, Observer
+from howl.tests.factories.observers import AlertFactory, ObserverFactory
 
 
 @pytest.mark.django_db
 class TestObserverAdmin:
 
-    def test_get_readonly_fields(self, rf):
+    def test_list_display(self, rf):
         modeladmin = ObserverAdmin(Observer, admin.site)
         assert modeladmin.list_display == ('name', 'operator', 'value')
+
+
+@pytest.mark.django_db
+class TestAlertAdmin:
+
+    def test_list_display(self, rf):
+        modeladmin = AlertAdmin(Alert, admin.site)
+        assert modeladmin.list_display == ('get_observer_name', 'timestamp', 'state')
+
+    def test_get_observer_name(self, rf):
+        observer = ObserverFactory.create()
+        alert = AlertFactory.create(observer=observer)
+
+        modeladmin = AlertAdmin(Alert, admin.site)
+        assert modeladmin.get_observer_name(alert) == observer.name
--- howl/tests/test_models.py
+++ howl/tests/test_models.py
@@ -1,5 +1,6 @@
 import pytest
 
+from howl.models import Alert
 from howl.tests.factories.observers import AlertFactory, ObserverFactory
 
 
@@ -19,6 +20,18 @@ class TestObserverModel:
     def test_is_exceeded(self, value, compare_value, return_value):
         obj = ObserverFactory.create(value=compare_value)
         obj.compare(value) is return_value
+
+    def test_check_and_alert(self):
+        obj = ObserverFactory.create(value=4)
+
+        assert Alert.objects.all().count() == 0
+        assert obj.check_and_alert(5) is True
+        assert Alert.objects.all().count() == 1
+
+    def test_check_and_alert_is_false(self):
+        obj = ObserverFactory.create(value=5)
+
+        assert obj.check_and_alert(5) is False
 
 
 @pytest.mark.django_db
