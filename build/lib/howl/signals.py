92ae4099376be4bdf47cf927182d5eabab701bcc
Benjamin Banduhn <benjamin.banduhn@moccu.com>
2015-12-06 15:42:33 +0100

    remove build and dist folder

--- build/lib/howl/signals.py
+++
@@ -1,6 +0,0 @@
-from django.dispatch import Signal
-
-
-howl_alert_warn = Signal(providing_args=['instance'])
-howl_alert_critical = Signal(providing_args=['instance'])
-howl_alert_delete = Signal(providing_args=['instance'])
