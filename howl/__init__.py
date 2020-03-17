ff00cf2b6edb744427a2e4f20b79a4d4afea9627
Benjamin Banduhn <ben@banduhn.com>
2020-03-17 12:58:28 +0100

    revert deprecation warning

--- howl/__init__.py
+++ howl/__init__.py
@@ -1,11 +1,2 @@
-import warnings
-
-
-warnings.warn((
-    'django-howl is not maintained anymore. Please use '
-    'django-pushover. (https://pypi.org/project/django-pushover/)'
-), DeprecationWarning)
-
-
 __version__ = '1.0.5'
 default_app_config = 'howl.apps.DjangoHowlConfig'
